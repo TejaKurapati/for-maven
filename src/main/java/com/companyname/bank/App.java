4b2063309f33558627df209a1d507d19d2a8ee8c
TejaKurapati <88663344+TejaKurapati@users.noreply.github.com>
2022-06-24 19:04:59 +0530

    automated build trigger 1

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "automated build trigger" );
+        System.out.println( "automated build trigger1" );
     }
 }
